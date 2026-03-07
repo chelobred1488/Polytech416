a63aca854791cfbef2e685c5c33f4d7c5876afca
Bakhtovar <ismatjonovB@outlook.com>
2026-03-07 06:56:04 +0500

    Added class Student and Specialty

---
+++ Lesson3/Specialty.cs
@@ -0,0 +1,8 @@
+namespace Lesson3;
+
+public class Specialty
+{
+    public string Id { get; }
+    public string Code { get; }
+    public string Describtion { get; }
+}
--- Lesson3/Student.cs
+++ Lesson3/Student.cs
@@ -2,5 +2,16 @@ namespace Lesson3;
 
 public class Student
 {
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string FamilyName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Phone { get; set; }
+    public string? Email { get; set; }
+    public string Address { get; set; }
+    public Specialty Specialty { get; set; }
+    public int YearsOfExperience { get; set; }
+    public bool isMerried { get; set; }
     
 }
